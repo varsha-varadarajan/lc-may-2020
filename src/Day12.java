57f165d3ab9b932c482608ed44592aea647aecf1
Varsha Varadarajan <nvvarsha2009@gmail.com>
2020-05-15 22:36:25 -0400

    Day 12 Add main method

--- src/Day12.java
+++ src/Day12.java
@@ -22,7 +22,11 @@ Binary Search
 */
 
 public class Day12 {
-    public int singleNonDuplicate(int[] nums) {
+    public static void main(String[] args) {
+        System.out.println(singleNonDuplicate(new int[] {1,1,2,3,3,4,4,8,8}));
+        System.out.println(singleNonDuplicate(new int[] {3,3,7,7,10,11,11}));
+    }
+    public static int singleNonDuplicate(int[] nums) {
         int low = 0;
         int high = nums.length-1;
         int mid = 0;
